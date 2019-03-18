5c6c9cdd2a7d4326dcdbc412aa5dd3edeee84500
Kirill Avramenko <kavramenko2k@gmail.com>
2019-03-18 18:07:12 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var PublisherChannels = make(map[string]*Channel)
+
+var SubscriberChannels = make(map[string]*Channel)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Channel struct {
+	PublisherToken    string      `json:"publisher_token"`
+	SubscriberToken   string      `json:"subscriber_token"`
+	SubscriberChannel chan string `json:"-"`
+}
+
+type Channels struct {
+	Channels []Channel `json:"channels"`
+}
+
+type Response struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/channel/prepare", PrepareChannel).Methods("GET")
+	router.HandleFunc("/channel", GetAllChannels).Methods("GET")
+	router.HandleFunc("/channel/subscribe/{subscriberToken}", CreateConnection)
+	router.HandleFunc("/channel/publish/{publisherToken}", PushToConnection).Methods("POST")
+
+	fmt.Println("Server started")
+	log.Fatal(http.ListenAndServe(":8844", router))
+}
+
+func GenerateChannel() Channel {
+	hasher := md5.New()
+	timeString := strconv.FormatInt(time.Now().Unix(), 10)
+	hasher.Write([]byte(timeString))
+	publisherKey := hex.EncodeToString(hasher.Sum(nil))
+
+	hasher.Write([]byte(publisherKey))
+	subscriberKey := hex.EncodeToString(hasher.Sum(nil))
+
+	return Channel{
+		PublisherToken:  publisherKey,
+		SubscriberToken: subscriberKey,
+	}
+}
+
+func PrepareChannel(w http.ResponseWriter, r *http.Request) {
+	var channel Channel
+	for {
+		channel = GenerateChannel()
+		if _, ok := PublisherChannels[channel.PublisherToken]; ok {
+			continue
+		} else {
+			PublisherChannels[channel.PublisherToken] = &channel
+			SubscriberChannels[channel.SubscriberToken] = &channel
+			break
+		}
+	}
+
+	preparedJson, _ := json.Marshal(channel)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(preparedJson)
+}
+
+func GetAllChannels(w http.ResponseWriter, r *http.Request) {
+	var allChannels []Channel
+	for _, value := range PublisherChannels {
+		allChannels = append(allChannels, *value)
+	}
+
+	preparedJson, _ := json.Marshal(Channels{
+		Channels: allChannels,
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(preparedJson)
+}
+
+func CreateConnection(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	if channel, ok := SubscriberChannels[vars["subscriberToken"]]; ok {
+		socketConnection, _ := upgrader.Upgrade(w, r, nil)
+		dataChannel := make(chan string)
+		channel.SubscriberChannel = dataChannel
+		go PushDataToConnection(socketConnection, dataChannel)
+	} else {
+		preparedJson, _ := json.Marshal(Response{
+			Message: "Subscriber token not found",
+			Type:    "ERR",
+		})
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(preparedJson)
+	}
+}
+
+func PushDataToConnection(socket *websocket.Conn, dataChan chan string) {
+	for {
+		select {
+		case data := <-dataChan:
+			_ = socket.WriteMessage(websocket.TextMessage, []byte(data))
+		}
+	}
+}
+
+func PushToConnection(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var response Response
+	if channel, ok := PublisherChannels[vars["publisherToken"]]; ok {
+		if channel.SubscriberChannel == nil {
+			response = Response{
+				Message: "Subscriber has not subscribed yet",
+				Type:    "INF",
+			}
+		} else {
+			channel.SubscriberChannel <- r.FormValue("data")
+			response = Response{
+				Message: "Successfully pushed data to subscriber",
+				Type:    "INF",
+			}
+		}
+	} else {
+		response = Response{
+			Message: "Publisher token not found",
+			Type:    "ERR",
+		}
+	}
+
+	preparedJson, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(preparedJson)
+}
